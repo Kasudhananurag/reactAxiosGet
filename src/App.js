516fe2be782b72ede8500ca8b839aca08d8f47c9
Kasudhananurag <114092146+Kasudhananurag@users.noreply.github.com>
2022-10-17 23:03:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import Axios from 'axios'
+import { useState, useEffect } from "react"
+
+function App() {
+
+  const [catFact, setCatFact] = useState("");
+
+  useEffect(() => {
+    Axios.get("https://catfact.ninja/fact").then((res) => {
+      setCatFact(res.data.fact);
+    });
+
+  }, [])
+
+
+return (
+  <>
+    <div>
+      <button  >Get Cat Fact</button>
+      <p> {catFact}  </p>
+    </div>
+  </>
+);
+}
+
+export default App;
